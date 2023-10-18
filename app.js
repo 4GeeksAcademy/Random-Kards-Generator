a163f318b4e720be9e775617ad4a0c293b162885
Omar Gutierrez Sanchez <117556558+spirosis@users.noreply.github.com>
2023-10-18 18:09:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ window.onload = () => {
 function deckBuilder() {
     const values = [ "A", "2", "3", "4", "5", "6", 
                     "7", "8", "9", "10", "J", "Q", "K", ];
-    const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+    const suits = ["♥", "♦", "♠", "♣"];
     const cards = [];
     for (let s = 0; s < suits.length; s++) {
       for (let v = 0; v < values.length; v++) {
@@ -21,24 +21,21 @@ function deckBuilder() {
       }
     } 
     return cards;
-  }
-  
+  } 
+// Declaring these load the cards from the begining. If removed it wont show card at all
   const cards = deckBuilder();
   randomCard(cards);
-  
+
   function randomCard(cards) {  
-    const random = Math.floor(Math.random() * 51);  
+    const random = Math.floor(Math.random() * 52);  
     const cardValue = cards[random].value;
     const cardSuit = cards[random].suit;
-    let entity;
-    cardSuit === "Diamonds" ? (entity = "&diams;") : (entity = "&" + cardSuit.toLowerCase() + ";");
     const card = document.createElement("div");
     card.classList.add("card", cardSuit.toLowerCase());
     card.innerHTML = 
-    '<span class="card-value-suit top">' + cardValue + entity + '</span>' + 
-    '<span class="card-suit">' + entity + '</span>' + 
-    '<span class="card-value-suit bot">' + cardValue + entity + '</span>';
+    '<span class="card-value-suit top">' + cardValue + cardSuit + '</span>' + 
+    '<span class="card-suit">' + cardValue + '</span>' + 
+    '<span class="card-value-suit bot">' + cardValue + cardSuit + '</span>';
     document.querySelector("#theDeck").innerHTML = "";
     document.querySelector("#theDeck").appendChild(card);
   }
-
